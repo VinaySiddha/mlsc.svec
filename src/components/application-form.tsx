a545ef18fd451c37fe6a108cd100a0f1e9d08079
Vinay Siddha <vinaysiddha.work@gmail.com>
2025-07-17 06:00:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/application-form.tsx
+++ src/components/application-form.tsx
@@ -20,6 +20,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
