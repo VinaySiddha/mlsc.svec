34ee11ebaaa36c000ab3636f5e1e0707be1a3f9e
Vinay Siddha <vinaysiddha.work@gmail.com>
2025-07-18 08:49:22 +0000

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ['latin'], variable: '--font-body' })
 export const metadata: Metadata = {
   title: 'MLSC 3.0 Hiring Program',
   description: 'Apply for the MLSC 3.0 Hiring Program. Join the Microsoft Learn Student Club and start your journey in tech. We are looking for passionate students for various technical and non-technical roles.',
-  keywords: ['MLSC 3.0', 'MLSC Hiring', 'Microsoft Learn Student Club', 'student club hiring', 'tech roles', 'SVEC'],
+  keywords: ['MLSC 3.0', 'MLSC 3.0 Hiring Program','MLSC Hiring', 'Microsoft Learn Student Club', 'student club hiring', 'tech roles', 'SVEC'],
   icons: {
     icon: '/logo.png',
   },
