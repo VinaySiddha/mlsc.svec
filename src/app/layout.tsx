69caeea34d85bfc394889cc94556bff4189419d7
Vinay Siddha <vinaysiddha.work@gmail.com>
2025-07-19 02:23:00 +0000

    Render blocking requests Est savings of 1,830 ms Requests are blocking t

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,11 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-      </head>
+      <head />
       <body className={cn('font-body antialiased', inter.variable)}>
         <main>{children}</main>
         <Toaster />
