1d82f3cde955f42dc6a90f563dac75d7442dd624
Vinay Siddha <vinaysiddha19@gmail.com>
2025-07-24 04:02:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <meta property="og:title" content="MLSC 3.0 Hiring Program X SVEC" />
         <meta property="og:site_name" content="MLSC SVEC" />
