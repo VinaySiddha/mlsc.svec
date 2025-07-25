57ad6cb376893a24213c54905b0362cd494c08e3
Vinay Siddha <vinaysiddha.work@gmail.com>
2025-07-25 18:15:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -3,7 +3,16 @@ const config = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 module.exports = config;
