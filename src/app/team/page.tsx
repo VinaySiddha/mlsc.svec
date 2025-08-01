a54312fa2d9ddea23819181df88e0f2e3fb51e9b
Vinay Siddha <vinaysiddha.work@gmail.com>
2025-08-01 11:38:41 +0000

    the team page is not updating dynamically its

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -8,6 +8,8 @@ import Link from "next/link";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 
+export const dynamic = 'force-dynamic';
+
 interface TeamMember {
   id: string;
   name: string;
