e445a81e24e2abb75b198ad1ee9ea6d7bd804c69
Vinay Siddha <vinaysiddha.work@gmail.com>
2025-08-01 11:43:14 +0000

    Error: Invalid src prop (https://drive.google.com/file/d/1AH2BQ795zT-jeM

--- next.config.js
+++ next.config.js
@@ -17,6 +17,12 @@ const config = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
