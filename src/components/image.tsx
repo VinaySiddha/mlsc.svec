8b5a177b6607da262df0288af8653c2168ee6b9b
Vinay Siddha <vinaysiddha.work@gmail.com>
2025-08-01 12:17:54 +0000

    https://drive.google.com/uc?export=view&id=1AH2BQ795zT-jeMwS1CzimRD1SD_Z

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,11 +1,20 @@
+
 "use client";
 
 import NextImage, { ImageProps } from 'next/image';
 import { convertGoogleDriveLink } from '@/lib/utils';
+import { cn } from '@/lib/utils';
 
 export const Image = (props: ImageProps) => {
-  const { src, ...rest } = props;
+  const { src, className, ...rest } = props;
+  const isGoogleDrive = typeof src === 'string' && src.includes('drive.google.com');
+
   const convertedSrc = typeof src === 'string' ? convertGoogleDriveLink(src) : src;
 
-  return <NextImage src={convertedSrc} {...rest} />;
+  if (isGoogleDrive) {
+    // eslint-disable-next-line @next/next/no-img-element
+    return <img src={convertedSrc as string} className={cn(className)} alt={props.alt} width={props.width} height={props.height} referrerPolicy="no-referrer" {...(rest as any)} />;
+  }
+
+  return <NextImage src={convertedSrc} className={className} {...rest} />;
 };
