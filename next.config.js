60592b227d8995e51f6b14820ce113841c582929
Komesh Bathula <komeshbathula@gmail.com>
2026-02-03 16:22:21 +0530

    Change message for confirmation mails and Change CSS of Events page

--- next.config.js
+++ next.config.js
@@ -26,7 +26,19 @@ const config = {
       },
       {
         protocol: 'https',
-        hostname: 'drive.googlee.com',
+        hostname: 'drive.google.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '1drv.ms',
         port: '',
         pathname: '/**',
       },
